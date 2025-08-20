bd9c9f859a24a19c88f762561db32ae8e393903d
nkm <km.nam@okestro.com>
2025-08-20 17:00:10 +0900

    bullet edited

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const GAME_CONFIG = {
     PLAYER_SIZE: 15,
     PLAYER_SPEED: 3,
     PLAYER_RUN_SPEED: 5,
-    BULLET_SPEED: 8,
-    BULLET_SIZE: 3,
+    BULLET_SPEED: 12,
+    BULLET_SIZE: 6,
     ENEMY_SIZE: 12,
     ENEMY_SPEED: 1,
     INITIAL_ENEMIES: 30,
@@ -59,7 +59,7 @@ const WEAPON_TYPES = {
         maxAmmo: 5,
         reloadTime: 3000,
         accuracy: 0.99,
-        range: 300
+        range: 400 // 사거리 늘림
     },
     SHOTGUN: {
         name: '샷건',
@@ -244,7 +244,7 @@ class Player {
         }
     }
 
-    shoot() {
+        shoot() {
         if (this.weapon.ammo > 0 && !this.weapon.isReloading && 
             Date.now() - this.weapon.lastShot > this.weapon.fireRate) {
             
@@ -254,14 +254,15 @@ class Player {
             // 샷건의 경우 여러 발의 펠릿 발사
             if (this.weapon.type === 'SHOTGUN') {
                 for (let i = 0; i < this.weapon.pellets; i++) {
-                    let spread = (Math.random() - 0.5) * 0.4; // 산탄 확산
+                    let spread = (Math.random() - 0.5) * 0.6; // 산탄 확산
                     bullets.push(new Bullet(
                         this.x + Math.cos(this.angle) * this.size,
                         this.y + Math.sin(this.angle) * this.size,
                         this.angle + spread,
                         'player',
                         this.weapon.damage / this.weapon.pellets, // 펠릿당 데미지
-                        this.weapon.range
+                        this.weapon.range,
+                        this.weapon.type
                     ));
                 }
     } else {
@@ -273,11 +274,17 @@ class Player {
                     this.angle + spread,
                     'player',
                     this.weapon.damage,
-                    this.weapon.range
+                    this.weapon.range,
+                    this.weapon.type
                 ));
             }
 
-            createMuzzleFlash(this.x, this.y, this.angle);
+            createMuzzleFlash(this.x, this.y, this.angle, this.weapon.type);
+            
+            // 탄약이 다 떨어지면 자동 재장전
+            if (this.weapon.ammo === 0 && this.weapon.totalAmmo > 0) {
+                this.reload();
+            }
         }
     }
 
@@ -300,13 +307,47 @@ class Player {
         ctx.translate(this.x - camera.x, this.y - camera.y);
         ctx.rotate(this.angle);
 
+        // 플레이어 몸체
         ctx.fillStyle = '#4CAF50';
         ctx.fillRect(-this.size/2, -this.size/2, this.size, this.size);
 
-        ctx.fillStyle = '#333';
-        ctx.fillRect(this.size/2, -2, this.size, 4);
+        // 무기별 다른 모양으로 그리기
+        this.drawWeapon();
 
         ctx.restore();
+    }
+
+    drawWeapon() {
+        let weaponLength = this.size;
+        let weaponWidth = 4;
+        
+        switch(this.weapon.type) {
+            case 'SNIPER_RIFLE':
+                ctx.fillStyle = '#2F4F4F'; // 어두운 회색
+                weaponLength = this.size * 1.5; // 더 긴 총
+                weaponWidth = 3;
+                break;
+            case 'SHOTGUN':
+                ctx.fillStyle = '#8B4513'; // 갈색
+                weaponLength = this.size * 1.2;
+                weaponWidth = 6; // 더 두꺼운 총
+                break;
+            case 'SMG':
+                ctx.fillStyle = '#000000'; // 검은색
+                weaponLength = this.size * 0.8; // 짧은 총
+                weaponWidth = 3;
+                break;
+            case 'LMG':
+                ctx.fillStyle = '#696969'; // 회색
+                weaponLength = this.size * 1.3;
+                weaponWidth = 7; // 가장 두꺼운 총
+                break;
+            default: // ASSAULT_RIFLE
+                ctx.fillStyle = '#333';
+                break;
+        }
+        
+        ctx.fillRect(this.size/2, -weaponWidth/2, weaponLength, weaponWidth);
     }
 }
 
@@ -341,7 +382,7 @@ class Enemy {
             if (distance < this.shootRange && Date.now() - this.lastShot > this.fireRate) {
                 this.shoot();
             }
-        } else {
+    } else {
             this.angle += (Math.random() - 0.5) * 0.2;
             this.x += Math.cos(this.angle) * GAME_CONFIG.ENEMY_SPEED * 0.3;
             this.y += Math.sin(this.angle) * GAME_CONFIG.ENEMY_SPEED * 0.3;
@@ -364,7 +405,10 @@ class Enemy {
             this.x + Math.cos(this.angle) * this.size,
             this.y + Math.sin(this.angle) * this.size,
             this.angle,
-            'enemy'
+            'enemy',
+            25,
+            150,
+            'ASSAULT_RIFLE'
         ));
     }
 
@@ -392,18 +436,50 @@ class Enemy {
 
 // 총알 클래스
 class Bullet {
-    constructor(x, y, angle, owner, damage = 25, range = 200) {
+    constructor(x, y, angle, owner, damage = 25, range = 200, weaponType = 'ASSAULT_RIFLE') {
         this.x = x;
         this.y = y;
         this.startX = x;
         this.startY = y;
         this.angle = angle;
-        this.speed = GAME_CONFIG.BULLET_SPEED;
-        this.size = GAME_CONFIG.BULLET_SIZE;
         this.owner = owner;
         this.damage = damage;
         this.range = range;
+        this.weaponType = weaponType;
         this.distanceTraveled = 0;
+        
+        // 무기별 총알 특성 설정
+        this.setBulletProperties();
+    }
+
+    setBulletProperties() {
+        switch(this.weaponType) {
+            case 'SNIPER_RIFLE':
+                this.speed = GAME_CONFIG.BULLET_SPEED * 1.8; // 저격총은 더 빠름
+                this.size = GAME_CONFIG.BULLET_SIZE + 2; // 더 큰 총알
+                this.color = '#FF4444'; // 빨간색
+                break;
+            case 'SHOTGUN':
+                this.speed = GAME_CONFIG.BULLET_SPEED * 0.8; // 샷건은 조금 느림
+                this.size = GAME_CONFIG.BULLET_SIZE + 1;
+                this.color = '#FFA500'; // 주황색
+                break;
+            case 'SMG':
+                this.speed = GAME_CONFIG.BULLET_SPEED * 1.2; // 기관단총은 빠름
+                this.size = GAME_CONFIG.BULLET_SIZE - 1;
+                this.color = '#00FF00'; // 초록색
+                break;
+            case 'LMG':
+                this.speed = GAME_CONFIG.BULLET_SPEED * 0.9;
+                this.size = GAME_CONFIG.BULLET_SIZE + 3; // 가장 큰 총알
+                this.color = '#8B4513'; // 갈색
+                break;
+            default: // ASSAULT_RIFLE
+                this.speed = GAME_CONFIG.BULLET_SPEED;
+                this.size = GAME_CONFIG.BULLET_SIZE;
+                this.color = this.owner === 'player' ? '#FFD700' : '#FF6666'; // 플레이어는 금색, 적은 연한 빨간색
+                break;
+        }
     }
 
     update() {
@@ -421,13 +497,27 @@ class Bullet {
     }
 
     draw() {
-        ctx.fillStyle = '#FFD700';
-        ctx.fillRect(
-            this.x - camera.x - this.size/2,
-            this.y - camera.y - this.size/2,
-            this.size,
-            this.size
-        );
+        ctx.fillStyle = this.color;
+        
+        // 저격총 총알은 더 눈에 띄게 표시
+        if (this.weaponType === 'SNIPER_RIFLE') {
+            ctx.shadowColor = this.color;
+            ctx.shadowBlur = 3;
+            ctx.fillRect(
+                this.x - camera.x - this.size/2,
+                this.y - camera.y - this.size/2,
+                this.size,
+                this.size
+            );
+            ctx.shadowBlur = 0;
+        } else {
+            ctx.fillRect(
+                this.x - camera.x - this.size/2,
+                this.y - camera.y - this.size/2,
+                this.size,
+                this.size
+            );
+        }
     }
 }
 
@@ -440,7 +530,7 @@ class Particle {
         this.size = size;
         this.velocity = velocity;
         this.life = 1.0;
-        this.decay = 0.02;
+        this.decay = 0.04; // 파티클이 더 빨리 사라지게
     }
 
     update() {
@@ -463,16 +553,39 @@ class Particle {
     }
 }
 
-function createMuzzleFlash(x, y, angle) {
-    for (let i = 0; i < 5; i++) {
+function createMuzzleFlash(x, y, angle, weaponType = 'ASSAULT_RIFLE') {
+    let particleCount = 3; // 파티클 수 줄임
+    let flashColor = '#FFD700';
+    
+    // 무기별 총구 화염 특성
+    switch(weaponType) {
+        case 'SNIPER_RIFLE':
+            particleCount = 2;
+            flashColor = '#FF4444';
+            break;
+        case 'SHOTGUN':
+            particleCount = 4;
+            flashColor = '#FFA500';
+            break;
+        case 'SMG':
+            particleCount = 2;
+            flashColor = '#00FF00';
+            break;
+        case 'LMG':
+            particleCount = 5;
+            flashColor = '#8B4513';
+            break;
+    }
+    
+    for (let i = 0; i < particleCount; i++) {
         particles.push(new Particle(
-            x + Math.cos(angle) * 20,
-            y + Math.sin(angle) * 20,
-            '#FFD700',
-            Math.random() * 6 + 2,
+            x + Math.cos(angle) * 15,
+            y + Math.sin(angle) * 15,
+            flashColor,
+            Math.random() * 4 + 1, // 크기 줄임
             {
-                x: Math.cos(angle + (Math.random() - 0.5) * 0.5) * (Math.random() * 3 + 1),
-                y: Math.sin(angle + (Math.random() - 0.5) * 0.5) * (Math.random() * 3 + 1)
+                x: Math.cos(angle + (Math.random() - 0.5) * 0.3) * (Math.random() * 2 + 0.5),
+                y: Math.sin(angle + (Math.random() - 0.5) * 0.3) * (Math.random() * 2 + 0.5)
             }
         ));
     }
